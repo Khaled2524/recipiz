3599a626f4ceac39d3f6260716a162bd6fbc71f7
khaled <khaledsalem2524@gmail.com>
2024-04-18 16:21:06 +0200

    First commit

---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import { View, Image, Text } from "react-native";
+
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      
+      <Text>Home Screen ABC</Text>
+    </View>
+  );
+}
